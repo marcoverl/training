3d5309195914cd3324bac427fd464ff2b2f7292a
marcoverl <marco.verlato@pd.infn.it>
2019-04-05 15:20:55 +0200

    Create copy-to-from-SE.sh
    testing copy  and delete commands with a SRM Storage Element

---
+++ Grid/copy-to-from-SE.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source /cvmfs/grid.cern.ch/etc/profile.d/setup-cvmfs-ui.sh
+voms-proxy-init -voms euindia
+sudo yum -y install libtool-ltdl
+export LCG_GFAL_INFOSYS=lcg-bdii.cern.ch:2170
+FILE=test-$(date +%d-%m-%Y-$RANDOM).txt
+echo test>$FILE
+lcg-cp file:///`pwd`/$FILE srm://grid2.fe.infn.it:8444/euindia/training/$FILE
+lcg-ls -l srm://grid2.fe.infn.it:8444/euindia/training/
+lcg-cp srm://grid2.fe.infn.it:8444/euindia/training/$FILE file:///`pwd`/copy-of-$FILE
+ls -l copy-of-$FILE
+lcg-del -l srm://grid2.fe.infn.it:8444/euindia/training/$FILE
+lcg-ls -l srm://grid2.fe.infn.it:8444/euindia/training/$FILE
+rm -f $FILE copy-of-$FILE

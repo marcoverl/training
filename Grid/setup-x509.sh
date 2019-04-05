f5c1b0c7fff66566d0ed390109e71b81e193dd9f
marcoverl <marco.verlato@pd.infn.it>
2019-04-05 15:37:40 +0200

    Update setup-x509.sh

--- Grid/setup-x509.sh
+++ Grid/setup-x509.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 mkdir .globus
 cat<<EOF>.globus/makepems.sh
-if [ "x$1" = "x" ]; then
-  SUPP_CERTS=`ls *.p12`
+if [ "x\$1" = "x" ]; then
+  SUPP_CERTS=\$(ls *.p12)
   echo "ERROR: certificate.p12 not specified"
   echo
   echo "USAGE: $0 <certificate.p12>"
-  echo "       Supported certs: $SUPP_CERTS "
+  echo "       Supported certs: \$SUPP_CERTS "
   echo "Exit."
   echo
   exit 1
 else
-  CERT=$1
+  CERT=\$1
 fi
-openssl pkcs12 -clcerts -nokeys -in $CERT -out usercert.pem
-openssl pkcs12 -nocerts -in $CERT -out userkey.pem
+openssl pkcs12 -clcerts -nokeys -in \$CERT -out usercert.pem
+openssl pkcs12 -nocerts -in \$CERT -out userkey.pem
 chmod 400 usercert.pem userkey.pem
 EOF
 chmod u+x .globus/makepems.sh

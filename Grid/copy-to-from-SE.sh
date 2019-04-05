4eb6929643041419c5f2094971b396f797f4ff7b
marcoverl <marco.verlato@pd.infn.it>
2019-04-05 16:28:44 +0200

    Update copy-to-from-SE.sh

--- Grid/copy-to-from-SE.sh
+++ Grid/copy-to-from-SE.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-source /cvmfs/grid.cern.ch/etc/profile.d/setup-cvmfs-ui.sh
-export VOMS_USERCONF=`pwd`/etc/grid-security/vomses
-export X509_VOMS_DIR=`pwd`/etc/grid-security/vomsdir
-voms-proxy-init -voms euindia
-sudo yum -y install libtool-ltdl
-export LCG_GFAL_INFOSYS=lcg-bdii.cern.ch:2170
 FILE=test-$(date +%d-%m-%Y-$RANDOM).txt
 echo test>$FILE
 lcg-cp file:///`pwd`/$FILE srm://grid2.fe.infn.it:8444/euindia/training/$FILE
